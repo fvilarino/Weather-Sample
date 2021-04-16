b11982b32e9d9aba62d5a6c28dc94bdb2a2bfb6b
Francesc J. Vilarino Guell <francesc@francescsoftware.com>
2021-04-15 21:31:21 -0700

    Fix up button on actionbar.

--- presentation/feature/src/main/java/com/francescsoftware/weathersample/presentation/feature/MainActivity.kt
+++ presentation/feature/src/main/java/com/francescsoftware/weathersample/presentation/feature/MainActivity.kt
@@ -3,10 +3,11 @@ package com.francescsoftware.weathersample.presentation.feature
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.francescsoftware.weathersample.presentation.feature.databinding.ActivityMainBinding
 import com.francescsoftware.weathersample.presentation.feature.navigator.Navigator
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var navigator: Navigator
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,14 +29,23 @@ class MainActivity : AppCompatActivity() {
         lifecycle.addObserver(navigator)
     }
 
-    override fun onSupportNavigateUp(): Boolean =
-        findNavController(R.id.nav_host_fragment).navigateUp()
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = getNavController()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 
     private fun configureToolbar() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
         setSupportActionBar(binding.toolbar)
+        val navController = getNavController()
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    private fun getNavController(): NavController {
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+        return navHostFragment.navController
     }
 }
