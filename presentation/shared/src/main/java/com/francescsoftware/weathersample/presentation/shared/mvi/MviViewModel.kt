3cf09dea3060db96a8d95a97b32e66657b0e4c85
Francesc Vilarino Guell <francesc@francescsoftware.com>
2021-04-27 13:14:14 -0700

    Add middleware to MVI framework.

--- presentation/shared/src/main/java/com/francescsoftware/weathersample/presentation/shared/mvi/MviViewModel.kt
+++ presentation/shared/src/main/java/com/francescsoftware/weathersample/presentation/shared/mvi/MviViewModel.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 interface State
@@ -23,9 +24,31 @@ interface ReduceAction
 private const val TAG = "MviViewModel"
 private const val FLOW_BUFFER_CAPACITY = 64
 
+enum class MiddlewareOrdering {
+    BEFORE_VIEWMODEL,
+    AFTER_VIEWMODEL
+}
+
+abstract class Middleware<I : MviIntent, R : ReduceAction> {
+    abstract val ordering: MiddlewareOrdering
+    private lateinit var processor: Processor<R>
+
+    abstract suspend fun executeIntent(intent: I)
+
+    fun handle(reduceAction: R) = processor.handle(reduceAction)
+
+    internal fun setProcessor(processor: Processor<R>) {
+        this.processor = processor
+    }
+}
+
+interface Processor<R : ReduceAction> {
+    fun handle(reduceAction: R)
+}
+
 abstract class MviViewModel<S : State, E : Event, I : MviIntent, R : ReduceAction>(
     initialState: S
-) : ViewModel() {
+) : ViewModel(), Processor<R> {
 
     private val stateFlow = MutableStateFlow<S>(initialState)
     private val eventFlow = MutableSharedFlow<E>(extraBufferCapacity = FLOW_BUFFER_CAPACITY)
@@ -37,11 +60,27 @@ abstract class MviViewModel<S : State, E : Event, I : MviIntent, R : ReduceActio
     private val intentFlow = MutableSharedFlow<I>(extraBufferCapacity = FLOW_BUFFER_CAPACITY)
     private val reduceFlow = MutableSharedFlow<R>(extraBufferCapacity = FLOW_BUFFER_CAPACITY)
 
+    protected open val middleware: List<Middleware<I, R>> = emptyList()
+
+    private val preMiddleware: List<Middleware<I, R>> by lazy(LazyThreadSafetyMode.NONE) {
+        middleware.filter { middleware ->
+            middleware.ordering == MiddlewareOrdering.BEFORE_VIEWMODEL
+        }
+    }
+
+    private val postMiddleware: List<Middleware<I, R>> by lazy(LazyThreadSafetyMode.NONE) {
+        middleware.filter { middleware ->
+            middleware.ordering == MiddlewareOrdering.AFTER_VIEWMODEL
+        }
+    }
+
     init {
         intentFlow
             .onEach { intent ->
                 Timber.tag(TAG).v("Processing [$intent]")
+                preMiddleware.forEach { middleware -> middleware.executeIntent(intent) }
                 executeIntent(intent)
+                postMiddleware.forEach { middleware -> middleware.executeIntent(intent) }
             }
             .launchIn(viewModelScope)
         reduceFlow
@@ -50,6 +89,11 @@ abstract class MviViewModel<S : State, E : Event, I : MviIntent, R : ReduceActio
                 stateFlow.value = reduce(stateFlow.value, action)
             }
             .launchIn(viewModelScope)
+
+        // run after constructor
+        viewModelScope.launch {
+            middleware.forEach { middleware -> middleware.setProcessor(this@MviViewModel) }
+        }
     }
 
     fun onIntent(intent: I) {
@@ -60,7 +104,7 @@ abstract class MviViewModel<S : State, E : Event, I : MviIntent, R : ReduceActio
         eventFlow.tryEmit(event)
     }
 
-    protected fun handle(reduceAction: R) {
+    override fun handle(reduceAction: R) {
         reduceFlow.tryEmit(reduceAction)
     }
 
