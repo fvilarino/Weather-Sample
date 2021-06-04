6d976f5c98b8b8c0716c107ee98cc0c03140bda2
Francesc Vilarino Guell <francesc@francescsoftware.com>
2021-06-03 17:33:56 -0700

    Replace inline classes with value classes.

--- utils/src/main/java/com/francescsoftware/weathersample/utils/time/Millis.kt
+++ utils/src/main/java/com/francescsoftware/weathersample/utils/time/Millis.kt
@@ -1,12 +1,14 @@
 package com.francescsoftware.weathersample.utils.time
 
-inline class Millis(val value: Long) {
+@JvmInline
+value class Millis(val value: Long) {
     companion object {
         fun convert(seconds: Seconds) = Millis(seconds.value * 1000L)
     }
 }
 
-inline class Seconds(val value: Long) {
+@JvmInline
+value class Seconds(val value: Long) {
     fun milliseconds(): Long = Millis.convert(this).value
 
     companion object {
