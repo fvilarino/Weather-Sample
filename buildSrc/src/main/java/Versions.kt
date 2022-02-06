df5b6f1625de038985e3cbc5966c8b7875246372
Francesc J. Vilarino Guell <francesc@francescsoftware.com>
2022-02-06 15:09:47 -0800

    Update dependencies.

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -9,11 +9,11 @@ object Versions {
     }
 
     object Plugin {
-        const val androidGradlePluginVersion = "7.2.0-alpha07"
-        const val navigationGradlePluginVersion = "2.4.0-rc01"
+        const val androidGradlePluginVersion = "7.3.0-alpha01"
+        const val navigationGradlePluginVersion = "2.5.0-alpha01"
         const val hiltGradlePluginVersion = "2.40.5"
         const val protobufPluginVersion = "0.8.12"
-        const val versionsPluginVersion = "0.39.0"
+        const val versionsPluginVersion = "0.42.0"
     }
 
     object Compose {
@@ -26,21 +26,21 @@ object Versions {
     object Kotlin {
         const val kotlinVersion = "1.6.10"
         const val coroutinesVersion = "1.6.0"
-        const val ktxVersion = "1.8.0-alpha02"
+        const val ktxVersion = "1.8.0-alpha03"
         const val kotlinSerializationVersion = "1.3.2"
     }
 
     object Android {
-        const val appCompatVersion = "1.4.0"
-        const val archLifeCycleViewModelVersion = "2.4.0"
-        const val lifecycleCommonVersion = "2.4.0"
+        const val appCompatVersion = "1.4.1"
+        const val archLifeCycleViewModelVersion = "2.5.0-alpha01"
+        const val lifecycleCommonVersion = "2.5.0-alpha01"
         const val navigationVersion = Plugin.navigationGradlePluginVersion
         const val dataStoreVersion = "1.0.0"
         const val androidXAnnotation = "1.3.0"
     }
 
     object Material {
-        const val materialVersion = "1.6.0-alpha01"
+        const val materialVersion = "1.6.0-alpha02"
     }
 
     object Google {
@@ -55,8 +55,8 @@ object Versions {
     }
 
     object Network {
-        const val okHttpVersion = "4.9.0"
-        const val okHttpInterceptorVersion = "4.9.0"
+        const val okHttpVersion = "5.0.0-alpha.4"
+        const val okHttpInterceptorVersion = "5.0.0-alpha.4"
         const val okIOVersion = "3.0.0"
         const val retrofitVersion = "2.9.0"
         const val retrofitAdapterVersion = "0.9.2"
@@ -69,8 +69,8 @@ object Versions {
 
     object Test {
         const val jUnitVersion = "4.13.2"
-        const val androidJUnitVersion = "1.1.3"
-        const val espressoCoreVersion = "3.4.0"
+        const val androidJUnitVersion = "1.1.4-alpha03"
+        const val espressoCoreVersion = "3.5.0-alpha03"
         const val coreTestingVersion = "2.1.0"
         const val coroutinesTestVersion = "1.4.3"
         const val mockkVersion = "1.11.0"
