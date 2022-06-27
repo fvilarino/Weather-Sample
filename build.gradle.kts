a2c1a920368835e3ac740e5daa57ec19218cb3cb
Francesc J. Vilarino Guell <francesc@francescsoftware.com>
2022-06-26 21:05:26 -0700

    chore: Add ktlint.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,31 @@ buildscript {
 plugins {
     alias(libs.plugins.com.github.ben.manes.versions)
     alias(libs.plugins.nl.littlerobots.version.catalog.update)
+    alias(libs.plugins.org.jlleitschuh.gradle.ktlint)
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    repositories {
+        mavenCentral()
+    }
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+        verbose.set(true)
+        android.set(true)
+        outputToConsole.set(true)
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.HTML)
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+        }
+        filter {
+            exclude("**/generated/**")
+            include("**/kotlin/**")
+        }
+    }
 }
 
 tasks.register("clean", Delete::class) {
