8f7f0b9f50a0b01864e8e54eab6ba2364ed6cb11
Francesc Vilariño Güell <francesc@francescsoftware.com>
2022-08-17 21:17:33 -0700

    chore: Update dependencies.

--- buildSrc/src/main/kotlin/base-hilt-compose-library.gradle.kts
+++ buildSrc/src/main/kotlin/base-hilt-compose-library.gradle.kts
@@ -1,22 +1,4 @@
 plugins {
     id("base-hilt-library")
-}
-
-val catalogs = extensions.getByType<VersionCatalogsExtension>()
-val composeVersion = catalogs
-    .named("libs")
-    .findVersion("androidx-compose-compiler-version")
-    .get()
-    .requiredVersion
-
-android {
-    buildFeatures {
-        compose = true
-    }
-    kotlinOptions {
-        freeCompilerArgs = freeCompilerArgs + Config.Compiler.composeFreeCompileArgs
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = composeVersion
-    }
+    id("base-compose-library")
 }
