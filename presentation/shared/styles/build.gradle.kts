9877dd98d898db221d62d4215604daab4cb8ab86
Francesc Vilariño Güell <francesc@francescsoftware.com>
2023-03-16 18:58:02 -0700

    feat: Add splash screen. (#28)

--- presentation/shared/styles/build.gradle.kts
+++ presentation/shared/styles/build.gradle.kts
@@ -13,4 +13,5 @@ dependencies {
     implementation(libs.androidx.compose.ui.ui)
     implementation(libs.androidx.compose.material3.material3)
     implementation(libs.com.google.android.material)
+    implementation(libs.androidx.core.core.splashscreen)
 }
