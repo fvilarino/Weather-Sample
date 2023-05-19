ac2afecd2bd08906df1540d428a05ee22bc3624f
Francesc Vilariño Güell <francesc@francescsoftware.com>
2023-05-18 17:35:25 -0700

    refactor: Use composable methods for title and icon on action bar.

--- ui/shared/composable/common/src/main/kotlin/com/francescsoftware/weathersample/ui/shared/composable/common/AppBar.kt
+++ ui/shared/composable/common/src/main/kotlin/com/francescsoftware/weathersample/ui/shared/composable/common/AppBar.kt
@@ -1,6 +1,5 @@
 package com.francescsoftware.weathersample.ui.shared.composable.common
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -8,22 +7,22 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 
 /**
  * App bar for all screens
  *
  * @param title - label to display on the app bar
- * @param iconId - navigation icon to display on the left corner
+ * @param icon - navigation icon to display on the left corner
  * @param iconContentDescription - accessibility label for the icon
- * @param onIconClick - called when the [iconId] is clicked
+ * @param onIconClick - called when the [icon] is clicked
  * @param modifier - the [Modifier] to apply to this app bar
  * @param actions - a list of action menu options to render on the right side of the app bar
  */
 @Composable
 fun AppBar(
     title: String,
-    @DrawableRes iconId: Int,
+    icon: ImageVector?,
     iconContentDescription: String?,
     onIconClick: () -> Unit,
     modifier: Modifier = Modifier,
@@ -31,10 +30,10 @@ fun AppBar(
 ) {
     TopAppBar(
         navigationIcon = {
-            if (iconId != 0) {
+            if (icon != null) {
                 IconButton(onClick = onIconClick) {
                     Icon(
-                        painter = painterResource(id = iconId),
+                        imageVector = icon,
                         contentDescription = iconContentDescription,
                     )
                 }
