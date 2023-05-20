d42d5eaaadfd6e14e6e7d845ac397ed4b507d028
Francesc Vilariño Güell <francesc@francescsoftware.com>
2023-05-20 10:28:12 -0700

    feat: Add support for dynamic theming.

--- ui/shared/styles/src/main/kotlin/com/francescsoftware/weathersample/ui/shared/styles/Theme.kt
+++ ui/shared/styles/src/main/kotlin/com/francescsoftware/weathersample/ui/shared/styles/Theme.kt
@@ -1,11 +1,16 @@
 package com.francescsoftware.weathersample.ui.shared.styles
 
+import android.os.Build
+import androidx.annotation.ChecksSdkIntAtLeast
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = DeepPurple500,
@@ -38,16 +43,24 @@ fun WeatherSampleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
-    val colors = if (darkTheme) {
-        DarkColorScheme
+    val colorScheme = if (supportsDynamicTheming()) {
+        val context = LocalContext.current
+        if (darkTheme) {
+            dynamicDarkColorScheme(context)
+        } else {
+            dynamicLightColorScheme(context)
+        }
     } else {
-        LightColorScheme
+        if (darkTheme) DarkColorScheme else LightColorScheme
     }
 
     MaterialTheme(
-        colorScheme = colors,
+        colorScheme = colorScheme,
         typography = Typography,
         shapes = Shapes,
         content = content,
     )
 }
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
+private fun supportsDynamicTheming() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
