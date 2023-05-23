1b02343ffae9b95846b38452c96fa83a53bca385
Francesc Vilariño Güell <francesc@francescsoftware.com>
2023-05-23 11:32:32 -0700

    feat: Hide bottom navigation bar on secondary destinations.

--- ui/feature/home/src/main/kotlin/com/francescsoftware/weathersample/ui/feature/home/WeatherApp.kt
+++ ui/feature/home/src/main/kotlin/com/francescsoftware/weathersample/ui/feature/home/WeatherApp.kt
@@ -1,14 +1,29 @@
 package com.francescsoftware.weathersample.ui.feature.home
 
 import android.app.Activity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -40,6 +55,7 @@ internal fun WeatherApp() {
             navGraph.getDestination(currentRoute)
         } ?: navGraphDestinations.first().rootDestination
         Scaffold(
+            contentWindowInsets = WindowInsets.statusBars,
             topBar = {
                 AppBar(
                     title = currentDestination.title,
@@ -49,60 +65,87 @@ internal fun WeatherApp() {
                     actions = { currentDestination.TopBarActions() },
                 )
             },
-            bottomBar = if (deviceClass == DeviceClass.Expanded) {
-                {}
-            } else {
+            bottomBar = if (deviceClass.hasBottomNavBar) {
                 {
-                    BottomNavBar(
-                        items = navGraphDestinations,
-                        currentDestination = navBackStackEntry?.destination,
-                        onClick = { destination ->
-                            navController.navigate(destination) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
+                    AnimatedVisibility(
+                        visible = currentDestination.showBottomBar,
+                        enter = slideInVertically { it } + fadeIn(),
+                        exit = slideOutVertically { it } + fadeOut(),
+                    ) {
+                        BottomNavBar(
+                            items = navGraphDestinations,
+                            currentDestination = navBackStackEntry?.destination,
+                            onClick = { destination ->
+                                navController.navigate(destination) {
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                    launchSingleTop = true
+                                    restoreState = true
                                 }
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        },
-                        modifier = Modifier.fillMaxWidth(),
-                    )
+                            },
+                            modifier = Modifier.fillMaxWidth(),
+                        )
+                    }
                 }
+            } else {
+                {}
             },
         ) { paddingValues ->
-            Row(
-                modifier = Modifier.padding(paddingValues),
-            ) {
-                if (deviceClass == DeviceClass.Expanded) {
-                    NavRail(
-                        items = navGraphDestinations,
-                        currentDestination = navBackStackEntry?.destination,
-                        onClick = { destination ->
-                            navController.navigate(destination) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
-                                }
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        },
-                    )
-                }
-                NavHost(
-                    navController,
-                    startDestination = navGraphDestinations.first().navGraphRoute,
-                    modifier = Modifier.weight(1f),
+            Box {
+                Row(
+                    modifier = Modifier.padding(paddingValues),
                 ) {
-                    addSearchNavGraph(
-                        deviceClass = deviceClass,
-                    ) { route ->
-                        navController.navigate(route)
+                    if (deviceClass == DeviceClass.Expanded) {
+                        NavRail(
+                            items = navGraphDestinations,
+                            currentDestination = navBackStackEntry?.destination,
+                            onClick = { destination ->
+                                navController.navigate(destination) {
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                    launchSingleTop = true
+                                    restoreState = true
+                                }
+                            },
+                        )
                     }
-                    addFavoritesNavGraph(
-                        deviceClass = deviceClass,
+                    NavHost(
+                        navController,
+                        startDestination = navGraphDestinations.first().navGraphRoute,
+                        modifier = Modifier.weight(1f),
+                    ) {
+                        addSearchNavGraph(
+                            deviceClass = deviceClass,
+                        ) { route ->
+                            navController.navigate(route)
+                        }
+                        addFavoritesNavGraph(
+                            deviceClass = deviceClass,
+                        )
+                    }
+                }
+                if (deviceClass.hasBottomNavBar && !currentDestination.showBottomBar) {
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .windowInsetsBottomHeight(WindowInsets.navigationBars)
+                            .fillMaxWidth()
+                            .background(
+                                Brush.verticalGradient(
+                                    colors = listOf(
+                                        Color.Transparent,
+                                        MaterialTheme.colorScheme.background,
+                                    )
+                                )
+                            )
                     )
                 }
             }
         }
     }
 }
+
+private val DeviceClass.hasBottomNavBar: Boolean
+    get() = this != DeviceClass.Expanded
