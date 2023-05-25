72da4e39dcfba646867ad7c864d4715cba282f84
Francesc Vilariño Güell <francesc@francescsoftware.com>
2023-05-25 09:19:23 -0700

    feat: Add navigation animations.

--- ui/feature/search/src/main/kotlin/com/francescsoftware/weathersample/ui/feature/search/navigation/Navigation.kt
+++ ui/feature/search/src/main/kotlin/com/francescsoftware/weathersample/ui/feature/search/navigation/Navigation.kt
@@ -1,5 +1,6 @@
 package com.francescsoftware.weathersample.ui.feature.search.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -27,27 +28,74 @@ fun NavGraphBuilder.addSearchNavGraph(
         startDestination = SearchRootDestination.rootDestination.route,
         route = SearchRootDestination.navGraphRoute,
     ) {
-        composable(
-            route = CitySearchDestination.route,
-        ) {
-            val cityViewModel: CityViewModel = hiltViewModel()
-            CityScreen(
-                viewModel = cityViewModel,
-                deviceClass = deviceClass,
-                onCityClick = { selectedCity -> onNavigate(WeatherDestination.getRoute(selectedCity)) },
-                modifier = Modifier.fillMaxSize(),
+        addSearchDestination(deviceClass, onNavigate)
+        addWeatherDestination(deviceClass)
+    }
+}
+
+private fun NavGraphBuilder.addSearchDestination(
+    deviceClass: DeviceClass,
+    onNavigate: (String) -> Unit,
+) {
+    composable(
+        route = CitySearchDestination.route,
+        enterTransition = {
+            val initialRoute = initialState.destination.route
+            val isInNavGraph = SearchRootDestination.destinations.any { destination ->
+                destination.route == initialRoute
+            }
+            if (isInNavGraph) {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.End,
+                )
+            } else {
+                null
+            }
+        },
+        exitTransition = {
+            val targetRoute = targetState.destination.route
+            val isInNavGraph = SearchRootDestination.destinations.any { destination ->
+                destination.route == targetRoute
+            }
+            if (isInNavGraph) {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Start,
+                )
+            } else {
+                null
+            }
+        },
+    ) {
+        val cityViewModel: CityViewModel = hiltViewModel()
+        CityScreen(
+            viewModel = cityViewModel,
+            deviceClass = deviceClass,
+            onCityClick = { selectedCity -> onNavigate(WeatherDestination.getRoute(selectedCity)) },
+            modifier = Modifier.fillMaxSize(),
+        )
+    }
+}
+
+private fun NavGraphBuilder.addWeatherDestination(deviceClass: DeviceClass) {
+    composable(
+        route = WeatherDestination.route,
+        deepLinks = WeatherDestination.deeplinks,
+        enterTransition = {
+            slideIntoContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Start,
             )
-        }
-        composable(
-            route = WeatherDestination.route,
-            deepLinks = WeatherDestination.deeplinks,
-        ) {
-            val weatherViewModel: WeatherViewModel = hiltViewModel()
-            WeatherScreen(
-                viewModel = weatherViewModel,
-                deviceClass = deviceClass,
-                modifier = Modifier.fillMaxSize(),
+        },
+        exitTransition = {
+            slideOutOfContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.End,
             )
-        }
+        },
+    ) {
+        val weatherViewModel: WeatherViewModel = hiltViewModel()
+        WeatherScreen(
+            viewModel = weatherViewModel,
+            deviceClass = deviceClass,
+            modifier = Modifier.fillMaxSize(),
+        )
     }
 }
