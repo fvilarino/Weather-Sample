bc83875407341385e78e2a39e66b2d4d03e82418
Francesc Vilariño Güell <francesc@francescsoftware.com>
2023-06-26 09:00:39 -0700

    build: Target API 34.

--- build-logic/convention/src/main/kotlin/com/francescsoftware/weathersample/buildconvention/Config.kt
+++ build-logic/convention/src/main/kotlin/com/francescsoftware/weathersample/buildconvention/Config.kt
@@ -5,8 +5,8 @@ object Config {
         const val VersionName = "1.0.0"
 
         const val MinSdk = 23
-        const val CompileSdk = 33
-        const val TargetSdk = 33
+        const val CompileSdk = 34
+        const val TargetSdk = 34
 
         val JavaVersion = org.gradle.api.JavaVersion.VERSION_17
     }
