5f04735af7a911d4b65a4f3e30af11a193f4fba0
Francesc Vilariño Güell <francesc@francescsoftware.com>
2023-07-18 13:07:12 -0700

    feat: Add Material3 app bar scroll behaviour.

--- ui/shared/composable/common/src/main/kotlin/com/francescsoftware/weathersample/ui/shared/composable/common/AppBar.kt
+++ ui/shared/composable/common/src/main/kotlin/com/francescsoftware/weathersample/ui/shared/composable/common/AppBar.kt
@@ -3,23 +3,24 @@ package com.francescsoftware.weathersample.ui.shared.composable.common
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 /**
  * App bar for all screens
  *
- * @param title - label to display on the app bar
- * @param icon - navigation icon to display on the left corner
- * @param iconContentDescription - accessibility label for the icon
- * @param onIconClick - called when the [icon] is clicked
- * @param modifier - the [Modifier] to apply to this app bar
- * @param actions - a list of action menu options to render on the right side of the app bar
+ * @param title label to display on the app bar
+ * @param modifier the [Modifier] to apply to this app bar
+ * @param scrollBehavior the app bar's [TopAppBarScrollBehavior] to handle color changes on scroll
+ * @param navigationIcon the navigation icon to display on the left of the app bar
+ * @param actions a composable with the action menu options to render on the right side of the app bar
  */
 @Composable
 fun AppBar(
     title: String,
     modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
     navigationIcon: @Composable () -> Unit = {},
     actions: @Composable RowScope.() -> Unit = {},
 ) {
@@ -32,5 +33,6 @@ fun AppBar(
         },
         actions = actions,
         modifier = modifier,
+        scrollBehavior = scrollBehavior,
     )
 }
