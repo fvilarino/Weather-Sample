aeadbba874b4a093d6d2b1f1ccb531e14ac9e28a
Francesc Vilariño Güell <francesc@francescsoftware.com>
2023-08-30 15:58:04 -0700

    feat: Remove system ui controller. (#221)
    * feat: Remove system ui controller.
    
    * chore: Detekt.

--- ui/feature/home/src/main/kotlin/com/francescsoftware/weathersample/ui/feature/home/MainActivity.kt
+++ ui/feature/home/src/main/kotlin/com/francescsoftware/weathersample/ui/feature/home/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.francescsoftware.weathersample.ui.feature.home
 
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.DisposableEffect
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.francescsoftware.weathersample.core.connectivity.api.ConnectivityMonitor
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
+@Suppress("MagicNumber")
+private val lightScrim = Color.argb(0xE6, 0xFF, 0xFF, 0xFF)
+
+@Suppress("MagicNumber")
+private val darkScrim = Color.argb(0x80, 0x1B, 0x1B, 0x1B)
 
 /** {@inheritDoc} */
 @AndroidEntryPoint
@@ -24,15 +32,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
+        enableEdgeToEdge()
         setContent {
-            val systemUiController = rememberSystemUiController()
             val darkTheme = isSystemInDarkTheme()
-
-            DisposableEffect(systemUiController, darkTheme) {
-                systemUiController.systemBarsDarkContentEnabled = !darkTheme
+            DisposableEffect(darkTheme) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(
+                        Color.TRANSPARENT,
+                        Color.TRANSPARENT,
+                    ) { darkTheme },
+                    navigationBarStyle = SystemBarStyle.auto(
+                        lightScrim,
+                        darkScrim,
+                    ) { darkTheme },
+                )
                 onDispose {}
             }
-
             WeatherApp(
                 connectivityMonitor = connectivityMonitor,
             )
