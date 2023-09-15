e5dca32004767c8b5817f6abf6fdcab3c7abca59
Francesc Vilariño Güell <francesc@francescsoftware.com>
2023-09-15 10:38:47 -0700

    refactor: Add catalog extension to build plug-ins. (#249)

--- build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
@@ -1,8 +1,7 @@
+import com.francescsoftware.weathersample.buildconvention.catalog
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
 
 class AndroidHiltConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -11,10 +10,9 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
                 apply("dagger.hilt.android.plugin")
             }
 
-            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
             dependencies {
-                add("implementation", libs.findLibrary("com.google.dagger.hilt.android").get())
-                add("ksp", libs.findLibrary("com.google.dagger.hilt.android.compiler").get())
+                add("implementation", catalog.findLibrary("com.google.dagger.hilt.android").get())
+                add("ksp", catalog.findLibrary("com.google.dagger.hilt.android.compiler").get())
             }
         }
     }
--- build-logic/convention/src/main/kotlin/KotlinTestLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/KotlinTestLibraryConventionPlugin.kt
@@ -1,21 +1,19 @@
+import com.francescsoftware.weathersample.buildconvention.catalog
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.api.tasks.testing.Test
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.named
 
 class KotlinTestLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
             dependencies {
-                add("testCompileOnly", libs.findLibrary("junit.junit").get())
-                add("testImplementation", libs.findLibrary("com.willowtreeapps.assertk.assertk").get())
-                add("testImplementation", libs.findLibrary("org.junit.jupiter.junit.jupiter").get())
-                add("testImplementation", libs.findLibrary("io.mockk").get())
+                add("testCompileOnly", catalog.findLibrary("junit.junit").get())
+                add("testImplementation", catalog.findLibrary("com.willowtreeapps.assertk.assertk").get())
+                add("testImplementation", catalog.findLibrary("org.junit.jupiter.junit.jupiter").get())
+                add("testImplementation", catalog.findLibrary("io.mockk").get())
             }
             tasks.named<Test>("test") {
                 useJUnitPlatform()
