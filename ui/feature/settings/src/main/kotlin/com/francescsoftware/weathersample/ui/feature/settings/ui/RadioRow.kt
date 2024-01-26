c1bc785cffeb9b1bb41ae6ca369640ca6b0784ca
Francesc Vilariño Güell <francesc@francescsoftware.com>
2024-01-26 13:33:15 -0800

    chore: Remove unused import.

--- ui/feature/settings/src/main/kotlin/com/francescsoftware/weathersample/ui/feature/settings/ui/RadioRow.kt
+++ ui/feature/settings/src/main/kotlin/com/francescsoftware/weathersample/ui/feature/settings/ui/RadioRow.kt
@@ -17,7 +17,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.francescsoftware.weathersample.ui.shared.styles.MarginDouble
 import com.francescsoftware.weathersample.ui.shared.styles.WeatherSampleTheme
